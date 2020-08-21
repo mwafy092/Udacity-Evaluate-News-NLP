70f03db84b4706a80c46cf43e59317fb1ea622ca
mwafy092 <mwafy092@gmail.com>
2020-08-21 17:52:11 +0200

    sass styles added

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,7 @@
 import { handleSubmit } from './js/formHandler'
 import { checkForName } from './js/nameChecker'
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
