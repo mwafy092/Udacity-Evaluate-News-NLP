2d069430e86821addd2904cce1e7d53764b8b1a7
mwafy092 <mwafy092@gmail.com>
2020-08-21 16:19:48 +0200

    html plugin installed / server modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     // entry point for webpack
@@ -12,5 +13,11 @@ module.exports = {
                 loader: 'babel-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        })
+    ]
 }
