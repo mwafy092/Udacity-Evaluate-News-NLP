22143e68552ceecf33d751f112faa1a2eba390e6
mwafy092 <mwafy092@gmail.com>
2020-08-22 19:37:35 +0200

    api added

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,5 @@
+import fetch from "node-fetch"
+
 export function handleSubmit(event) {
     event.preventDefault()
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,18 @@
-var path = require('path')
+// express and app instance
 const express = require('express')
+const app = express()
+
+var path = require('path')
 const mockAPIResponse = require('./mockAPI.js')
+
+// meaning cloud
+const MeaningCloud = require('meaning-cloud')
+
+// env file config
 const dotenv = require('dotenv');
+const { default: fetch } = require('node-fetch')
 dotenv.config();
 
-const app = express()
 
 app.use(express.static('dist'))
 
@@ -15,11 +23,37 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-let port = 8081;
+let port = 8000;
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`)
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    res.send(mockAPIResponse);
+})
+
+/*
+    sentiment API 
+*/
+
+// using our API KEY
+let appData = [];
+let API_KEY = process.env.API_KEY;
+let baseURL = `https://api.meaningcloud.com/sentiment-2.1?key=${API_KEY}&lang=en&txt=<text>&model=general`
+
+let getAPIData = async (baseURL) => {
+    const request = await fetch(baseURL);
+    try {
+        const data = await request.json();
+        // console.log(data);
+        appData.push(data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+getAPIData(baseURL);
+
+app.get('/data', (request, response) => {
+    response.send(appData);
 })
